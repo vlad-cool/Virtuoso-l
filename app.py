6b9c1de25ed1303355c2d668300dcf30b7968b0e
vlad <vlad-matveev2004@mail.ru>
2022-07-23 01:30:03 +0300

    cleanup

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3 
 import kivy
 from kivy.core.window import Window
 from kivy.app import App
@@ -10,9 +11,9 @@ import os
 #Config.set('graphics', 'width', '480')
 #Config.set('graphics', 'height', '1920')
 #Config.set('graphics', 'borderless', '0')
+#Config.set('kivy', 'show_fps', '1')
 
 Config.set('kivy', 'exit_on_escape', '1') #Debug
-#Config.set('kivy', 'show_fps', '1')
 
 Config.set('modules', 'monitor', '')
 
@@ -23,7 +24,7 @@ if os.name != "nt":
 
 kivy.require('2.1.0')  
 
-#kivy.core.window.WindowBase(borderless="1")
+kivy.core.window.WindowBase(borderless="0")
 
 class KivyApp(App):
     def build(self):
