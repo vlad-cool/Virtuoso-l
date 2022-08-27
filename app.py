e60154866b8c55719e10fc7602030957e9be07ed
vlad <vlad-matveev2004@mail.ru>
2022-08-27 23:49:41 +0300

    made some work

--- app.py
+++ app.py
@@ -5,18 +5,21 @@ from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.app import App
 import platform
-import pickle
-#import get_address.py
+import subprocess
+from collections import deque
+from kivy.core.text import LabelBase
 
-read_interval = .1 #(seconds) time between trying to read data from gpio_in pipe
-global app_config #app config dictionaty
-global gpio_in
+send_queue = deque()
+read_interval = .2
+send_interval = .9
+rc5_address = 0
+toggle_bit = 1
+weapon = 0
+carousel_codes = [[2, 3, 2], [9, 15, 9]]
+carousel_indexes = [0, 0]
 
-try:
-    with (open("config", "rb")) as f: #loading app config
-        app_config = pickle.load(f)
-except:
-    app_config = {"weapon" : 1} #default config dictionary
+with open("./rc5_address", "r") as file:
+    rc5_address = int(file.readline())
 
 if platform.machine() == "armv7l": #for bananapi, it have much better performance when running vertically
     Window.rotation = 90
@@ -24,18 +27,41 @@ if platform.machine() == "armv7l": #for bananapi, it have much better performanc
 kivy.require('2.1.0')
 
 class KivyApp(App):
+    def carousel_btn_handler(a, carousel_number):
+        global carousel_indexes
+        global carousel_codes
+        global send_queue
+        send_queue.append(str(rc5_address * (2**6) + carousel_codes[carousel_number][2]))
+
+    def carousel_handler(a, new_index, carousel_number):
+        global carousel_indexes
+        global carousel_codes
+        global send_queue
+        send_queue.append(str(rc5_address * (2**6) + carousel_codes[carousel_number][(3 + new_index - carousel_indexes[carousel_number]) % 3 - 1]))
+        carousel_indexes[carousel_number] = new_index % 3
+
+    def set_weapon(a, new_weapon):
+        if platform.machine() != "armv7l":
+            print(f"weapon: {new_weapon}")
+            return
+        global weapon
+
+        if (3 + new_weapon - weapon) % 3 == 1:
+            subprocess.run(["sudo", "./weapon", "1"])
+
+        if (3 + new_weapon - weapon) % 3 == 2:
+            subprocess.run(["sudo", "./weapon", "2"])
+
+        weapon = new_weapon
+
     def build(self):
         return Builder.load_file('main.kv')
 
-    def on_start(self):
-        app = App.get_running_app()
-        #app.root.ids[f"weapon_{weapon}"].state = "down"
-
-    def on_stop(self):
-        pass
-
-
 def get_data(dt):
+    global weapon
+    global send_queue
+    if len(send_queue) > 0:
+        return
     data = []
     app = App.get_running_app()
     for i in range(9):
@@ -51,8 +77,13 @@ def get_data(dt):
                 a //= 2
             if j == 0 and data[0][3] == 1:
                 break
-        else:
-            app.root.ids["test_output"].text = str(data)[1:-1].replace(", ", "").replace("[", "").replace("]", "\n") #debug output
+        app.root.ids["test_output"].text = str(data)[1:-1].replace(", ", "").replace("[", "").replace("]", "\n") #debug output
+
+    if 3 - data[0][7] * 2 - data[0][6] < 3:
+        for i in range(3):
+            app.root.ids[f"weapon_{i}"].state = "normal"
+        weapon = (1 - data[0][7]) * 2 + 1 - data[0][6]
+        app.root.ids[f"weapon_{weapon}"].state = "down"
 
     if data[8][0] == 0:
         return
@@ -62,18 +93,10 @@ def get_data(dt):
         score_r *= 2
         score_r += i
 
-    score_r = str(score_r)
-    if len(score_r) == 1:
-        score_r = "  " + score_r
-
     score_l = data[7][3]
     for i in data[5][3:]:
         score_l *= 2
         score_l += i
-
-    score_l = str(score_l)
-    if len(score_l) == 1:
-        score_l = score_l + "  "
 
     timer_m = 0
     timer_d = 0
@@ -93,12 +116,6 @@ def get_data(dt):
         timer_s += i
 
     timer = f"{timer_m}:{timer_d}{timer_s}"
-
-    if 3 - data[0][7] * 2 - data[0][6] < 3:
-        for i in range(3):
-            app.root.ids[f"weapon_{i}"].state = "normal"
-        weapon = (1 - data[0][7]) * 2 + 1 - data[0][6]
-        app.root.ids[f"weapon_{weapon}"].state = "down"
 
     if data[8][5]:
         app.root.ids["warning_bot_l"].state = "down"
@@ -120,11 +137,37 @@ def get_data(dt):
         app.root.ids["warning_bot_r"].state = "normal"
         app.root.ids["warning_top_r"].state = "normal"
 
-    app.root.ids["score_l"].text = score_l
+    if score_l < 10:
+        app.root.ids["score_l_l"].text = str(score_l)
+    else:
+        app.root.ids["score_l_l"].text = str(score_l // 10)
+        app.root.ids["score_l_r"].text = str(score_l % 10)
+
+    if score_r < 10:
+        app.root.ids["score_r_r"].text = str(score_r)
+    else:
+        app.root.ids["score_r_l"].text = str(score_l // 10)
+        app.root.ids["score_r_r"].text = str(score_l % 10)
+
     app.root.ids["score_r"].text = score_r
     app.root.ids["timer"].text = timer
 
+def send_data(dt):
+    global send_queue
+    global toggle_bit
+
+    if len(send_queue) > 0:
+        if platform.machine() != "armv7l":
+            print(send_queue[0])
+            send_queue.popleft()
+            return
+        subprocess.run(["sudo", "./output", send_queue[0], str(toggle_bit)])
+        toggle_bit = 1 - toggle_bit
+        send_queue.popleft()
 
 if __name__ == "__main__":
+    LabelBase.register(name="agencyb", fn_regular='AGENCYB.TTF')
+    LabelBase.register(name="agencyr", fn_regular='AGENCYR.TTF')
     Clock.schedule_interval(get_data, read_interval)
+    Clock.schedule_interval(send_data, send_interval)
     KivyApp().run()
