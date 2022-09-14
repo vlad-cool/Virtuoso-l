af51c0d030506866b98ed8fd87351244dfd8d7c3
vlad <vlad-matveev2004@mail.ru>
2022-09-15 00:51:57 +0300

    refactored!

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import time
 import serial
 import get_address
 import gpio_control
+from kivy.clock       import Clock
 from kivy.core.window import Window
 from kivy.lang        import Builder
-from kivy.clock       import Clock
 from kivy.app         import App
 from kivy.core.text   import LabelBase
 from collections      import deque
@@ -27,17 +27,17 @@ class KivyApp(App):
             address_file.write(f"{rc5_address}\n")
 
     def send_handler(self, code):
-        self.send_queue.append(str(self.rc5_address * (2**6) + code))
+        self.send_queue.append(self.rc5_address * (2**6) + code)
 
     def carousel_handler(self, a, old_index, new_index, commands):
         self.send_handler(commands[(2 + new_index - old_index) % 3])
         
-    def set_weapon(self, a, new_weapon):
+    def set_weapon(self, new_weapon):
         if machine() != "armv7l":
             print(f"weapon: {new_weapon}")
             return
 
-        gpio_control.button_emu(37, (3 + new_weapon - self.weapon) % 3)
+        gpio_control.button_emu(37, (3 + new_weapon - self.root.weapon) % 3)
         self.weapon = new_weapon
 
     def change_weapon_connection_type(a):
@@ -48,47 +48,28 @@ class KivyApp(App):
         gpio_control.button_emu(27, 1)
 
     def send_data(self, dt):
-        if self.send_proc is not None and self.send_proc.poll() is None:
-            return
+        #if self.send_proc is not None and self.send_proc.poll() is None:
+        #    return
         if len(self.send_queue) > 0:
             if machine() != "armv7l":
                 print(self.send_queue[0])
                 self.send_queue.popleft()
                 return
             #self.send_proc = subprocess.Popen(f"sudo ./output {self.send_queue[0]} {self.toggle_bit}", shell=True)
+            gpio_control.ir_emu(self.send_queue[0], self.toggle_bit)
             self.toggle_bit = 1 - self.toggle_bit
             self.send_queue.popleft()
 
-    def byte_to_arr(byte):
+    def byte_to_arr(self, byte):
         a = [0] * 8
         for i in range(8):
-                byte[i] = a % 2
+                a[i] = byte % 2
                 byte //= 2
         return a[::-1]
 
-    def get_data(self, dt):
-        data = [] * 8
+    def data_update(self, data):
+        root = self.root
 
-        if machine() == "armv7l":
-            while self.data_rx.inWaiting // 8 > 0:
-                for i in range(8):
-                    byte = int.from_bytes(self.data_rx.read(), "big")
-                    data[byte // 2 ** 5] = self.byte_to_arr(byte)
-        
-            self.weapon                 = gpio_control.read_pin(32) * 2 + gpio_control.read_pin(36)
-            self.video_timer            = gpio_control.read_pin(18)
-            self.weapon_connection_type = gpio_control.read_pin(7)
-
-        else:
-            data = [[0, 0, 0, 0, 0, 0, 0, 0],
-                    [0, 0, 1, 0, 0, 0, 0, 0],
-                    [0, 1, 0, 0, 0, 0, 0, 0],
-                    [0, 1, 1, 0, 0, 0, 0, 0],
-                    [1, 0, 0, 0, 0, 0, 0, 0],
-                    [1, 0, 1, 0, 0, 0, 0, 0],
-                    [1, 1, 0, 0, 0, 0, 0, 0],
-                    [1, 1, 1, 0, 0, 0, 0, 0],]
-        
         score_r = data[7][3]
         for i in data[5][3:]:
             score_r *= 2
@@ -100,18 +81,18 @@ class KivyApp(App):
             score_l += i
 
         if score_l < 10:
-            self.score_l_l = str(score_l)
-            self.score_l_r = " "
+            root.score_l_l = str(score_l)
+            root.score_l_r = " "
         else:
-            self.score_l_l = str(score_l // 10)
-            self.score_l_r = str(score_l % 10)
+            root.score_l_l = str(score_l // 10)
+            root.score_l_r = str(score_l % 10)
 
         if score_r < 10:
-            self.score_r_l = " "
-            self.score_r_r = str(score_r)
+            root.score_r_l = " "
+            root.score_r_r = str(score_r)
         else:
-            self.score_r_l = str(score_r // 10)
-            self.score_r_r = str(score_r % 10)
+            root.score_r_l = str(score_r // 10)
+            root.score_r_r = str(score_r % 10)
 
         timer_m = 0
         timer_d = 0
@@ -129,13 +110,15 @@ class KivyApp(App):
             timer_s *= 2
             timer_s += i
         
-        if self.timer_3 != str(timer_s):
-            self.flash_timer = time.time()
+        if self.old_sec != str(timer_s):
+            root.flash_timer = time.time()
+            pass
 
-        self.timer_0 = str(timer_m)
-        self.timer_2 = str(timer_d)
-        self.timer_3 = str(timer_s)
-        self.timer_running = data[2][3]
+        self.old_sec = str(timer_s)
+        root.timer_0 = str(timer_m)
+        root.timer_2 = str(timer_d)
+        root.timer_3 = str(timer_s)
+        root.timer_running = data[2][3]
 
         period = 0
         
@@ -144,62 +127,65 @@ class KivyApp(App):
             period += data[6][4 + i]
 
         if period == 15:
-            self.priority = 1
+            root.priority = 1
         elif period == 14:
-            self.priority = -1
+            root.priority = -1
         elif period == 13:
-            self.priority = 0
+            root.priority = 0
         elif period >= 1 and period <= 9:
-            self.priority = 0
-            self.period = period
+            root.priority = 0
+            root.period = period
             
-        if self.passive_timer == -1 and self.timer_running == 1:
-            self.passive_timer = time.time()
-        elif self.passive_timer != -1 and self.timer_running == 0:
-            self.passive_timer = -1
+        if root.passive_timer == -1 and root.timer_running == 1:
+            root.passive_timer = time.time()
+        elif root.passive_timer != -1 and root.timer_running == 0:
+            root.passive_timer = -1
 
-        if self.passive_timer != -1:
-            current_time = int(time.time() - self.passive_timer)
+        if root.passive_timer != -1:
+            current_time = int(time.time() - root.passive_timer)
 
-            self.passive_yel_size = min(self.passive_yel_max_size * current_time // 30, self.passive_yel_max_size)
-            self.passive_red_size = min(max(self.passive_red_max_size * (current_time - 30) // 30, 0), self.passive_red_max_size)
+            root.passive_yel_size = min(self.passive_yel_max_size * current_time // 30, self.passive_yel_max_size)
+            root.passive_red_size = min(max(self.passive_red_max_size * (current_time - 30) // 30, 0), self.passive_red_max_size)
 
-        self.warning_l = data[7][4] * 2 + data[7][5]
-        self.warning_r = data[7][7] * 2 + data[7][6]
+        root.warning_l = data[7][4] * 2 + data[7][5]
+        root.warning_r = data[7][6] * 2 + data[7][7]
 
-    def build(self):
-        self.send_queue = deque()
-        self.toggle_bit = 1
-        self.rc5_address = 0
-        self.send_proc = None
 
-        self.weapon = 0
-        self.weapon_connection_type = 0
+    def get_data(self, dt):
+        self.root.current_time = time.time()
+        if machine() == "armv7l":
+            while self.data_rx.inWaiting() // 8 > 0:
+                data = [[0] * 8] * 8
+                for i in range(8):
+                    byte = int.from_bytes(self.data_rx.read(), "big")
+                    data[byte // 2 ** 5] = self.byte_to_arr(byte)
+
+                self.data_update(data)
+
+            self.root.weapon                 = gpio_control.read_pin(32) * 2 + gpio_control.read_pin(36)
+            self.root.video_timer            = gpio_control.read_pin(18)
+            self.root.weapon_connection_type = gpio_control.read_pin(7)
+
+        else:
+            data = [[0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 1, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 0, 0, 0, 0, 0],
+                    [1, 0, 1, 0, 0, 0, 1, 1],
+                    [1, 1, 0, 0, 0, 0, 0, 0],
+                    [1, 1, 1, 0, 0, 1, 1, 0],]
+            self.data_update(data)
         
-        self.video_timer = 0
+    def build(self):
+        self.send_proc            = None
+        self.send_queue           = deque()
+        self.toggle_bit           = 1
+        self.rc5_address          = 0
+        self.old_sec = "0"
 
         self.passive_yel_max_size = 40
         self.passive_red_max_size = 40
-        self.passive_yel_size = 0
-        self.passive_red_size = 0
-        self.passive_timer = -1
-
-        self.score_l_l = "0"
-        self.score_l_r = " "
-        self.score_r_l = " "
-        self.score_r_r = "0"
-
-        self.timer_0 = "0"
-        self.timer_1 = ":"
-        self.timer_2 = "0"
-        self.timer_3 = "0"
-
-        self.flash_timer = time.time()
-        self.timer_running = 0
-        self.period = 0
-        self.priority = 0
-        self.warning_l = 0
-        self.warning_r = 0
 
         self.color_left_score     = [0.8, 0.0, 0.0, 1]
         self.color_right_score    = [0.0, 0.8, 0.0, 1]
@@ -217,6 +203,8 @@ class KivyApp(App):
     def on_start(self):
         Clock.schedule_interval(self.get_data, read_interval)
         Clock.schedule_interval(self.send_data, send_interval)
+        self.root.flash_timer  = time.time()
+        self.root.current_time = time.time()
         with open("./rc5_address", "r") as address_file:
             self.rc5_address = int(address_file.readline())
 
--- gpio_control.py
+++ gpio_control.py
@@ -1,14 +1,14 @@
 from threading import Thread
 from ctypes    import *
-from time      import sleep
+from time      import sleep, time_ns
 from platform  import machine
 
+off_time = 250
+on_time  = 250
+TIMING = 889
+
 if machine() == "armv7l":
-
-    off_time = 250
-    on_time  = 250
-
-    gpio = cdll.LoadLibrary("wiringPi.h")
+    gpio = cdll.LoadLibrary("/usr/lib/libwiringPi.so")
 
     gpio.wiringPiSetupPhys()
 
@@ -54,11 +54,37 @@ def button_emu(pin, times):
         gpio.digitalWrite(pin, 0)
         sleep(off_time / 1000)
         gpio.digitalWrite(pin, 1)
-        sleep(on_time  / 100)
+        sleep(on_time  / 1000)
 
 @run_in_thread
-def ir_emu(code):
-    pass
+def ir_emu(to_transmit, toggle_bit):
+    print(to_transmit)
+    data = [0] * 14
+    to_transmit += 12288
+    to_transmit += toggle_bit * 2048
+
+    for i in range(14):
+        data[13 - i] = to_transmit % 2
+        to_transmit //= 2
+
+    print(data)
+
+    for i in range(14):
+        gpio.digitalWrite(26, 0 + data[i])
+        
+        t = time_ns() // 1000
+
+        while time_ns() // 1000 - t < TIMING:
+            pass
+        
+        t = time_ns() // 1000
+
+        gpio.digitalWrite(26, 1 - data[i])
+
+        while time_ns() // 1000 - t < TIMING:
+            pass
+
+    gpio.digitalWrite(26, 1)
 
 def read_pin(pin):
     return gpio.digitalRead(pin)
