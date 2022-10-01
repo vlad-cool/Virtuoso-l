3479035e7740f03a1eb3d62a3a94321e8d26deea
vlad <vlad-matveev2004@mail.ru>
2022-10-01 23:51:23 +0300

    kivy installation fixed (2)

--- install_libs.sh
+++ install_libs.sh
@@ -63,7 +63,7 @@ sudo apt -y autoremove
 
 mkdir -p V24m
 cd V24m
-mkdir tmp
+pi@bananapim2zero:~/V24m$ sudo mount -o remount,size=4G /tmp/
 python3 -m venv venv
 venv/bin/python3 -m pip install --upgrade pip setuptools virtualenv
 venv/bin/python3 -m pip install Pillow
@@ -72,7 +72,7 @@ venv/bin/python3 -m pip install setuptools
 venv/bin/python3 -m pip install cython
 venv/bin/python3 -m pip install clang
 venv/bin/python3 -m pip install ffpyplayer
-TMPDIR=tmp venv/bin/python3 -m pip install kivy
+venv/bin/python3 -m pip install kivy
 rm -rf tmp
 sudo chown root:users venv/bin/python3
 sudo chmod ug+s venv/bin/python3
