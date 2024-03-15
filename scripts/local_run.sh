ecff30cc88227e30b1e83129ebdd195b8cfebf2b
vlad-cool <vlad-matveev2004@mail.ru>
2024-03-15 18:12:03 +0300

    added 360 layout

--- scripts/local_run.sh
+++ scripts/local_run.sh
@@ -4,12 +4,15 @@
 export IS_BANANA=False
 export INPUT_SUPPORT=True
 export CONFIG_FILE=config.json
-export KIVY_FILE=main1920x480.kv
+export KIVY_FILE=main1920x360.kv
 ## Video
 export VIDEO_SUPPORT=True
 export VIDEO_PATH=/home/vlad/Documents/VSCode/V24m/test_videos
 export VIDEO_PATH_TMP=./video/tmp
 export VIDEO_ENCODER=libx264
 
-cd src
+cd src/template
+make
+
+cd ..
 ../venv/bin/python3 app.py
