76d1d668e6e365d73d16073f4cf8328998f7c563
vlad-cool <vlad-matveev2004@mail.ru>
2024-03-15 18:55:12 +0300

    settings parametrization

--- scripts/local_run.sh
+++ scripts/local_run.sh
@@ -4,12 +4,14 @@
 export IS_BANANA=False
 export INPUT_SUPPORT=True
 export CONFIG_FILE=config.json
-export KIVY_FILE=main1920x360.kv
+export KIVY_FILE=main1920x480.kv
 ## Video
 export VIDEO_SUPPORT=True
 export VIDEO_PATH=/home/vlad/Documents/VSCode/V24m/test_videos
 export VIDEO_PATH_TMP=./video/tmp
 export VIDEO_ENCODER=libx264
+
+set -e
 
 cd src/template
 make
